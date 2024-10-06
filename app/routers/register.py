60c2cc21c8aebd5aa436d1cd7742e442dfc59c50
Precious Steve <precioussteve01@gmail.com>
2024-10-06 01:23:46 +0100

    feat: implement async keyword

--- app/routers/register.py
+++ app/routers/register.py
@@ -8,8 +8,8 @@ from app.crud.user_crud import get_user_by_username, get_user_by_email, create_u
 router = APIRouter(tags=["Authentication"], prefix="/auth")
 
 
-@router.post("/register", description="Endpoint to create new user")
-def create_new_user(user:user_schema.UserCreate, session:Session = Depends(get_db)):
+@router.post("/register/", description="Endpoint to create new user")
+async def create_new_user(user:user_schema.UserCreate, session:Session = Depends(get_db)):
     existing_user_by_email = get_user_by_email(session, user.email)
     if existing_user_by_email:
         raise HTTPException(status_code=409, detail="User already exists")
