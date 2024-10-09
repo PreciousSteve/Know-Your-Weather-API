7bab223da3249082267de601fca19747ffd2a339
Precious Steve <precioussteve01@gmail.com>
2024-10-09 01:23:51 +0100

    feat: add create function for new location

---
+++ app/schemas/location_schema.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Location(BaseModel):
+    name: str
+    latitude: float
+    longitude: float
+    description: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+
+class LocationResponse(Location):
+    id: int
